843ced4e61d8ee391df87949c682b5098e476525
Lemming122 <kay550733@gmail.com>
2026-06-15 02:37:27 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -14,9 +14,9 @@ def get_auth_headers():
     return {"Content-Type": "application/json", "X-SecretKey": SECRET_KEY}
 
 
-@app.route('https://ro69.vercel.app/api/TD', methods=['POST'])
+@app.route('/api/TD', methods=['POST'])
 def titled_data():
-    return jsonify({"MOTD":" Put your motd u want like (WELCOME TOO GOOBA TAG OUR OWNERS ARE HARMONY AND AVII ) "})
+    return jsonify({"MOTD":" Put your motd u want like (WELCOME TOO GOOBA TAG OUR OWNERS ARE HARMONY AND AVII) "})
 
 @app.route("/api/PlayFabAuthentication", methods=["POST"])
 def playfab_authentication():
